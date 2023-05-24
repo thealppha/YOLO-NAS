8e652a84f806859303925479961ffb986ba6a5b7
Tunahan Ulusoy <tunaeem@gmail.com>
2023-05-24 17:59:41 +0300

    added YOLO-NAS inference code and installation steps

--- inference.py
+++ inference.py
@@ -4,39 +4,52 @@ import torch
 from super_gradients.training import models
 from torchinfo import summary
 
-device = 'cuda' if torch.cuda.is_available() else "cpu"
-
-image_flag = False
-video_flag = True
-
-yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
-
-summary(model=yolo_nas_l, 
-        input_size=(16, 3, 640, 640),
-        col_names=["input_size", "output_size", "num_params", "trainable"],
-        col_width=20,
-        row_settings=["var_names"],
-        verbose=False
-)
-
-if image_flag:
-        url = "https://previews.123rf.com/images/freeograph/freeograph2011/freeograph201100150/158301822-group-of-friends-gathering-around-table-at-home.jpg"
-        yolo_nas_l.predict(url, conf=0.25).show()
-
-
-if video_flag:
-        video_id_list = ["HgFGjJqmU0U", "KCsPNKjHFuY", "zKDfhGpRy3w"]
-
-        for video_id in video_id_list:
-                video_url = f'https://www.youtube.com/watch?v={video_id}'  
-                if not os.path.exists('videos/%(title)s.%(ext)s'):
-                        command = f"python3 -m youtube_dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' {video_url} -o 'videos/%(title)s.%(ext)s'"
-                        os.system(command)
+class Inference():
         
-        videos = [file for file in os.listdir("videos/") if file.endswith("mp4")]
+        def __init__(self, model, video_id):
+                self.model = model             
+                self.video_id = video_id
 
-        for video in videos:
-                input_video_path = f"videos/{video}"
-                output_video_path = f"detections/{video}"
+        def get_device(self):
+                device = 'cuda' if torch.cuda.is_available() else "cpu"
 
-                yolo_nas_l.to(device).predict(input_video_path).save(output_video_path)
+                return device
+
+        def get_model(self):
+                model = models.get(self.model, pretrained_weights="coco")
+
+                summary(model=model, 
+                        input_size=(16, 3, 640, 640),
+                        col_names=["input_size", "output_size", "num_params", "trainable"],
+                        col_width=20,
+                        row_settings=["var_names"],
+                        verbose=False
+                )
+                
+                return model
+        
+        def get_video(self):
+                video_url = f'https://www.youtube.com/watch?v={self.video_id}'  
+
+                command = f"python3 -m youtube_dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' {video_url}"
+                os.system(command)
+
+                video_name = [file for file in os.listdir() if file.endswith("mp4")][0]
+
+                return video_name
+
+        def main(self):
+                device = self.get_device()
+                video = self.get_video()
+                model = self.get_model()
+
+                input_video_path = video
+                output_video_path = f"detection/{video}"
+
+                model.to(device).predict(input_video_path).save(output_video_path)
+
+if __name__ == "__main__":
+        inference = Inference("yolo_nas_l", "HgFGjJqmU0U")
+        inference.main()
+
+        os.remove([file for file in os.listdir() if file.endswith("mp4")][0])
