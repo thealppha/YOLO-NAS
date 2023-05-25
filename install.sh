25eb68bad1c84e61c1725175f5989fd7a2e34d3b
Tunahan Ulusoy <tunaeem@gmail.com>
2023-05-25 11:26:57 +0300

    added requirements.txt

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pip install --user super-gradients==3.1.0 imutils roboflow torchinfo 
-pip install --user -U pytube
-pip install --user -U "git+https://github.com/ytdl-org/youtube-dl.git"
